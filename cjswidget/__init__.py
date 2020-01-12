8e3b73b695bfdfa02057115ae91956575b22d57d
Luke Rogers <luke@dmptr.com>
2020-01-13 03:22:07 +1300

    [0.4.0] Configurable options, code style updates!

--- cjswidget/__init__.py
+++ cjswidget/__init__.py
@@ -1,4 +1,4 @@
 """choices.js widget for Django"""
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 default_app_config = "cjswidget.apps.CJSWidgetConfig"
--- cjswidget/widgets.py
+++ cjswidget/widgets.py
@@ -1,8 +1,28 @@
 from django.forms import widgets
 
+import shortuuid
+
 
 class CJSWidget(widgets.Select):
     template_name = 'cjswidget/widget.html'
+
+    def __init__(self, options=None, **kwargs):
+        super().__init__(**kwargs)
+        self.uuid = shortuuid.uuid()
+
+        if options is None:
+            options = {}
+
+        self.options = options
+        self.options_id = 'options_' + self.uuid
+
+    def get_context(self, *args):
+        context = super().get_context(*args)
+        context.update({
+            'options': self.options,
+            'options_id': self.options_id,
+        })
+        return context
 
     class Media:
         js = (

7ba07cbe36fe78f87c4b9bd84fcb7e04578d3f69
Luke Rogers <luke@dmptr.com>
2026-03-05 00:20:51 +1300

    remove default_app_config, update metadata
    - Remove deprecated default_app_config (Django 3.2+)
    - Update classifiers to Python 3.10-3.13
    - Update requires-python to >=3.10
    - Bump version to 0.5.0

--- cjswidget/__init__.py
+++ cjswidget/__init__.py
@@ -1,4 +1,2 @@
 """A replacement for the Select widget with fancy new features - powered by choices.js!"""
-__version__ = '0.4.1'
-
-default_app_config = "cjswidget.apps.CJSWidgetConfig"
+__version__ = '0.5.0'
